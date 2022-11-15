481838cebb3ee26b73407e584c4f97f7e73f3570
Justin-110 <tianji@ad.unc.edu>
2022-11-15 17:52:00 -0500

    A3-09

--- bin/cli.js
+++ bin/cli.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist'
-import { roll_dice } from "/lib/roll.js"
+import {roll} from "/lib/roll.js"
 
 const args = minimist(process.argv.slice(2))
 
 // create constants for sides, dice, and rolls.
-const sides_arg = args.sides || 6
-const dice_arg = args.sides || 2
-const rolls_arg = args.rolls || 1
+const sides = args.sides || 6
+const dice = args.sides || 2
+const rolls = args.rolls || 1
 
 // print out the results using stringify
-console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
+console.log(JSON.stringify(roll(sides, dice, rolls)))
 // all done!
 process.exit(0);
