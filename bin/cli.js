3388d8c3567844f577a2f41af6906c45fb3f05d8
Justin-110 <tianji@ad.unc.edu>
2022-11-30 11:33:19 -0500

    A03-14

--- bin/cli.js
+++ bin/cli.js
@@ -5,12 +5,7 @@ import {roll} from "/lib/roll.js"
 
 const args = minimist(process.argv.slice(2))
 
-// create constants for sides, dice, and rolls.
-const sides = args.sides || 6
-const dice = args.dice || 2
-const rolls = args.rolls || 1
-
 // print out the results using stringify
-console.log(JSON.stringify(roll(sides, dice, rolls)))
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
 // all done!
 process.exit(0);
