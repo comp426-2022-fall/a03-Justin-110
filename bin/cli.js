1b2c7dd04c299e63de8cabfd425c45aef42d09d2
Justin-110 <tianji@ad.unc.edu>
2022-11-30 11:39:08 -0500

    A03-15

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
 import minimist from 'minimist'
-import {roll} from "/lib/roll.js"
+import {roll} from "../lib/roll.js"
 
 const args = minimist(process.argv.slice(2))
 
 // print out the results using stringify
 console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
-// all done!
-process.exit(0);
