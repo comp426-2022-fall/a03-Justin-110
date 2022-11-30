a59b5a6382202ef2370c2c67694e6cc3dd6f6cdf
Justin-110 <tianji@ad.unc.edu>
2022-11-30 11:28:45 -0500

    A03-update

--- bin/cli.js
+++ bin/cli.js
@@ -7,7 +7,7 @@ const args = minimist(process.argv.slice(2))
 
 // create constants for sides, dice, and rolls.
 const sides = args.sides || 6
-const dice = args.sides || 2
+const dice = args.dice || 2
 const rolls = args.rolls || 1
 
 // print out the results using stringify
